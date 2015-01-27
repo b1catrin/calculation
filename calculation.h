bbbec7b63d4c7d3217c4c6ebbe9feaa18492b8d7
b1catrin <catrin.busley@b1-es.com>
2015-01-27 10:48:47 +0100

    initial add in

---
+++ calculation.h
@@ -0,0 +1,31 @@
+/*
+ * calculation.h
+ *
+ *  Created on: 20.01.2015
+ *      Author: busley
+ */
+
+#ifndef CALCULATION_H_
+#define CALCULATION_H_
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+
+
+#define KEY "Enter the calculator Operation you want to do:"
+
+// Function prototype declaration
+int addition(int a, int b);
+int subtraction(int a, int b);
+int multiplication(int a, int b);
+int division(int a, int b);
+int modulus(int a, int b);
+void calculator_operations();
+
+
+
+
+
+#endif /* CALCULATION_H_ */
---
+++ src/calculation.c
@@ -0,0 +1,86 @@
+/*
+ * calculation.c
+ *
+ *  Created on: 20.01.2015
+ *      Author: busley
+ */
+
+#include "..\calculation.h"
+
+
+//Function Definitions
+
+void calculator_operations()
+{
+    //system("cls");  use system function to clear
+    //screen instead of clrscr();
+	printf("\n             Welcome to C calculator \n\n");
+
+	printf("******* Press 'Q' or 'q' to quit ");
+	printf("the program ********\n");
+	printf("***** Press 'H' or 'h' to display ");
+	printf("below options *****\n\n");
+	printf("Enter 'C' or 'c' to clear the screen and");
+	printf(" display available option \n\n");
+
+	printf("Enter + symbol for Addition \n");
+	printf("Enter - symbol for Subtraction \n");
+	printf("Enter * symbol for Multiplication \n");
+	printf("Enter / symbol for Division \n");
+    printf("Enter ? symbol for Modulus\n");
+}
+
+int addition(int a, int b)
+{
+	int total=0;
+
+	total = a + b;
+	printf("\n%d + %d = %d\n", a, b, total);
+	fflush(stdout);
+	return total;
+}
+
+int subtraction(int a, int b)
+{
+	int total=0;
+
+    total = a - b;
+    printf("\n%d - %d = %d\n", a, b, total);
+    fflush(stdout);
+    return total;
+}
+
+int multiplication(int a, int b)
+{
+	int total=0;
+
+    total=a*b;
+    printf("\n%d * %d = %d\n", a, b, total);
+    fflush(stdout);
+    return total;
+}
+
+int division(int a, int b)
+{
+	int total=0;
+
+    if (b != 0)
+    {
+    	total=a/b;
+    }
+    printf("\n%d / %d = %d\n", a, b, total);
+	fflush(stdout);
+	return total;
+}
+
+
+int modulus(int a, int b)
+{
+    int total=0;
+
+    total=a%b;
+    printf("\nModulus of entered numbers = %d\n",total);
+    fflush(stdout);
+    return total;
+}
+
